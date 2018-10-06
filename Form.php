526166bd6b6b3010f7e2e915aa0a139628970c91
ANTONIO <ANTONIO@192.168.1.44>
2018-10-05 22:56:07 -0500

    Creando formulario Antonio Alcantara

---
+++ Form.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>LISTA:</title>
+ </head>
+ <body>
+     <h1>LISTA DE NUMEROS</h1>
+     <?php
+     include("metodo.php");
+        echo numeros();
+        ?>
+ </body>
+</html>
